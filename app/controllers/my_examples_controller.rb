0f8c2744ae39d218c3c753d04f692c8125d2360c
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-10-20 14:35:21 -0500

    add bottles

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -13,6 +13,17 @@ class MyExamplesController < ApplicationController
       numbers.push(number)
       count += 1
     end
-    render json: numbers
+    render json: numbers.join(" ")
+  end
+
+  def bottles
+    count = 99
+    song = []
+    while count > 1
+      song.push("#{count} bottles of beer on the wall. #{count} bottles of beer! Take one down pass it around.")
+      count -= 1
+    end
+    song.push("1 bottle of beer on the wall. 1 bottle of beer! Take one down pass it around. No more bottles of beer on the wall.")
+    render json: song
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/fortune", controller: "my_examples", action: "fortune"
   get "/lotto", controller: "my_examples", action: "lotto"
+  get "/bottles", controller: "my_examples", action: "bottles"
 end
